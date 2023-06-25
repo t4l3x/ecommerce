39fbf4568829a07aaf85bbb21f992546b81f7921
Taleh <t4lex999@gmail.com>
2023-06-25 16:19:02 +0400

    Laravel main app moved to infrastructure

--- app/Infrastructure/Laravel/Http/Middleware/EncryptCookies.php
+++ app/Infrastructure/Laravel/Http/Middleware/EncryptCookies.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Infrastructure\Laravel\Http\Middleware;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
--- app/Infrastructure/Laravel/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Infrastructure/Laravel/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Infrastructure\Laravel\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
+use App\Infrastructure\Laravel\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;

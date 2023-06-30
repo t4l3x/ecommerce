437d78eb9d865a3cf19673ebf07a77856d1a306c
Taleh <t4lex999@gmail.com>
2023-06-30 13:41:21 +0400

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\App\Domain\IdentityAccess\User\Entities\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        // \App\Models\App\Domain\IdentityAccess\User\Entities\User::factory()->create([
+        //     'name' => 'Test App\Domain\IdentityAccess\User\Entities\User',
         //     'email' => 'test@example.com',
         // ]);
     }
